24b0da6f1534258a2f3a933c347b04808dee9445
Winicius-Neves <winiciusneves2004@hotmail.com>
2024-06-02 11:05:23 -0300

    first commit

---
+++ fundamentos-react/src/controller/model/index.js
@@ -0,0 +1,8 @@
+
+export default function PrimerioComponente() {
+    return (
+        <div>
+            <h1>Primeiro componente</h1>
+        </div>
+    )
+}
---
+++ fundamentos-react/src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import './styles.css';
+
+
+import PrimerioComponente from './controller/model/index';
+
+ReactDOM.render(<div> <PrimerioComponente /> </div>, document.getElementById('root'));

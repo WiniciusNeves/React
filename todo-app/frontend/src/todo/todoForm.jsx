9e36a0302075ed9744800cef69c38ec05544c19a
Winicius-Neves <winiciusneves2004@hotmail.com>
2024-08-04 23:42:22 -0300

    update

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -6,19 +6,26 @@ import { IoMdClose } from "react-icons/io";
 
 import Grid from "../template/grid";
 export default (props) => {
+
+    const keyHandler = (e) => {
+        if (e.key === 'Enter') {
+            e.shiftKey ? props.handleSearch() : props.handleAdd();
+        } else if (e.key === 'Escape') {
+            props.handleClear();
+        }
+    }
+
     return (
         <form className="todoForm row p-3">
             <Grid cols="12 9 10  ">
-                <input type="text" id="description" className="form-control" placeholder="Adicionar Tarefa" value={props.description} onChange={props.handleChange} />
+                <input type="text" id="description" className="form-control" placeholder="Adicionar Tarefa" value={props.description} onChange={props.handleChange} 
+                onKeyUp={keyHandler} />
             </Grid>
 
             <Grid cols="12 3 2">
                 <IconButton style="primary" icon={<FaPlus />} onClick={props.handleAdd} />
                 <IconButton style="info" icon={<FaSearch />} onClick={props.handleSearch} />
-
-                <IconButton style="danger" icon={<IoMdClose />} onClick={props.handleClear} />
-
-
+                <IconButton style="default border   " icon={<IoMdClose />} onClick={props.handleClear} />
             </Grid>
 
         </form >
